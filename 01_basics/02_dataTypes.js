1fd3b754129bab9a6d321ebc1ae168c924aa150a
Raz03 <92332663+Raz03@users.noreply.github.com>
2025-03-21 07:25:31 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict" // treat all JS code as newer version
+
+// alert (3+3) //we are using nodejs, not browser
+
+console.log(3 
+    +3) // code readability should be high
+
+let state;
+console.log(state);
+
+
+// number ==> 2 to power of 53
+// bigint
+// string ==> ""
+// boolean ==> true/false
+// null ==> standalone value
+// undefined ==> value not assigned
+// symbol ==> unique
+
+// primitive DT
+//object
+
+console.log(typeof "Razack");
+console.log(typeof 18);
+console.log(typeof null); // null is an object
+console.log(typeof undefined); // undefined type
