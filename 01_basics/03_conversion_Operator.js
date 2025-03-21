27a6528d4d31149e0ed1231cff6dc228d6115fc6
Raz03 <92332663+Raz03@users.noreply.github.com>
2025-03-21 11:18:19 +0000

    coversion of datatypes

---
+++ 01_basics/03_conversion_Operator.js
@@ -0,0 +1,35 @@
+// let score = 33
+// let score = "33"
+// let score = "33abc"
+//let score = null
+//let score = undefined
+//let score = true
+
+//console.log(typeof score);
+//console.log(typeof score);
+
+//let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+//let isLoggedIn = 1
+//let isLoggedIn = 0
+//let isLoggedIn = ""
+//let isLoggedIn = "Razack"
+
+//let booleanIsLoggeedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggeedIn);
+
+// 1 => true; 0=> false
+// "" => false
+// "hitesh" => true
+
+let randomNumber = 18
+
+let stringNumber = String(randomNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
