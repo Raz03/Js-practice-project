b72e4a3f380a3c7267e27601f3cd35e55bc0175e
Raz03 <92332663+Raz03@users.noreply.github.com>
2025-04-22 09:03:32 +0000

    datatypes summary

--- 01_basics/03_conversion_Operator.js
+++ 01_basics/03_conversion_Operator.js
@@ -31,5 +31,39 @@
 let randomNumber = 18
 
 let stringNumber = String(randomNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// ----------Operations----------
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(21%2)
+// console.log(22/2)
+
+let str1 = "Hello "
+let str2 = "Razack"
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log(1 + "2")
+// console.log("1" + 2)
+// console.log(1 + "2" + 2)
+// console.log("1" + "2" + 2)
+// console.log(1 + 2 + "2")
+// console.log("1" + 2 + 2)
+// console.log((1 + 2) * 4 / 2)
+
+// console.log(true)
+// console.log(+true)
+// console.log(+false)
+
+let num1, num2, num3
+num1 = num2 = num3 = 2+2;
+console.log(num1+num3+num2+num1-num2-num2-num2-num1-num2-num3)
